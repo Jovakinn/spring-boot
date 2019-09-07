bfa7a650139c4c9569c268220d20e0adb507eb00
38098 <max05012004@gmail.com>
2019-09-07 11:41:26 +0200

    Add unworked CartDAOTest and CartServiceTest

---
+++ src/test/java/com/mainacad/service/CartServiceTest.java
@@ -0,0 +1,47 @@
+package com.mainacad.service;
+
+import com.mainacad.ApplicationRunner;
+import com.mainacad.entity.Cart;
+import com.mainacad.entity.Status;
+import com.mainacad.entity.User;
+import com.mainacad.service.interfaces.CartService;
+import com.mainacad.service.interfaces.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringJUnitConfig(ApplicationRunner.class)
+@ActiveProfiles("dev")
+class CartServiceTest {
+
+    @Autowired
+    CartService cartService;
+    UserService userService;
+
+    @Test
+    public void testGetAndUpdate() {
+        User user = new User();
+        userService.save(user);
+
+        Cart cart = new Cart();
+        cart.setStatus(Status.DELIVERED);
+        cart.setTime(1l);
+        cart.setUser(user);
+
+        cartService.save(cart);
+
+        Cart savedCart = cartService.findOne(cart.getId());
+
+        assertEquals(savedCart.getUser(), user);
+        savedCart.setStatus(Status.OPEN);
+
+        Cart updatedCart = cartService.update(savedCart);
+
+        assertEquals(updatedCart.getStatus(), Status.OPEN);
+    }
+
+}
