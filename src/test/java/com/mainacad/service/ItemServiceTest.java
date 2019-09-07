090fa67422f84b62b6c3f537920cb2dd0439e3a3
38098 <max05012004@gmail.com>
2019-09-07 09:56:52 +0200

    Add ItemServiceTest

---
+++ src/test/java/com/mainacad/service/ItemServiceTest.java
@@ -0,0 +1,40 @@
+package com.mainacad.service;
+
+import com.mainacad.ApplicationRunner;
+import com.mainacad.entity.Item;
+import com.mainacad.service.interfaces.ItemService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringJUnitConfig(ApplicationRunner.class)
+@ActiveProfiles("dev")
+class ItemServiceTest {
+
+    @Autowired
+    ItemService itemService;
+
+    @Test
+    public void testGetAndUpdate() {
+        Item item = new Item();
+        item.setPrice(10000);
+        item.setName("Machina");
+        item.setInitPrice(2222);
+        item.setArticle("Perf");
+
+        itemService.save(item);
+
+        Item savedItem = itemService.findOne(item.getId());
+
+        assertEquals(savedItem.getName(), "Machina");
+        savedItem.setArticle("new_article");
+
+        Item updatedItem = itemService.update(savedItem);
+
+        assertEquals(updatedItem.getArticle(), "new_article");
+    }
+}
